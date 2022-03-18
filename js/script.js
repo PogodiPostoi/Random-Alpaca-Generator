d0cf80e517db01d334344fa64bcbf3e4698eda4f
Artyom Pogodin <boombox577@gmail.com>
2022-03-18 16:47:50 +0300

    layout, optimise old and new code for style

--- js/script.js
+++ js/script.js
@@ -1,27 +1,17 @@
 //  reference: https://github.com/Samuel-Abusa/Alpaca-Generator
 
 const menu = document.getElementById('menu__toggle')
-const hair = document.querySelector('.alpaca__configurator-elements-hair')
-const ears = document.querySelector('.alpaca__configurator-elements-ears')
-const eyes = document.querySelector('.alpaca__configurator-elements-eyes')
-const mouth = document.querySelector('.alpaca__configurator-elements-mouth')
-const neck = document.querySelector('.alpaca__configurator-elements-neck')
-const leg = document.querySelector('.alpaca__configurator-elements-leg')
-const accessories = document.querySelector('.alpaca__configurator-elements-accessories')
-const background = document.querySelector('.alpaca__configurator-elements-background')
 
-const hairStyle = document.querySelector('.alpaca__configurator-style--hair')
-const earsStyle = document.querySelector('.alpaca__configurator-style--ears')
-const eyesStyle = document.querySelector('.alpaca__configurator-style--eyes')
-const mouthStyle = document.querySelector('.alpaca__configurator-style--mouth')
-const neckStyle = document.querySelector('.alpaca__configurator-style--neck')
-const legStyle = document.querySelector('.alpaca__configurator-style--leg')
-const accessoriesStyle = document.querySelector('.alpaca__configurator-style--accessories')
-const backgroundStyle = document.querySelector('.alpaca__configurator-style--background')
-
-const configElements = document.querySelector('.alpaca__configurator-elements')
-const styleElements = document.querySelectorAll('.alpaca__configurator-style')
-
+const configElements = Array.from(document.querySelectorAll('.alpaca__configurator-elements'))
+const styleElements = Array.from(document.querySelectorAll('.alpaca__configurator-style'))
+const styleElementsHairArray = Array.from(document.querySelectorAll('.alpaca__configurator-style-hair'))
+const styleElementsEarsArray = Array.from(document.querySelectorAll('.alpaca__configurator-style-ears'))
+const styleElementsEyesArray = Array.from(document.querySelectorAll('.alpaca__configurator-style-eyes'))
+const styleElementsMouthArray = Array.from(document.querySelectorAll('.alpaca__configurator-style-mouth'))
+const styleElementsNeckArray = Array.from(document.querySelectorAll('.alpaca__configurator-style-neck'))
+const styleElementsLegArray = Array.from(document.querySelectorAll('.alpaca__configurator-style-leg'))
+const styleElementsAccessoriesArray = Array.from(document.querySelectorAll('.alpaca__configurator-style-accessories'))
+const styleElementsBackgroundArray = Array.from(document.querySelectorAll('.alpaca__configurator-style-background'))
 
 
 // Бургер-меню. Запрет на скролл при активном меню
@@ -29,78 +19,86 @@ menu.addEventListener('click', () =>  document.body.classList.toggle('noScroll')
 
 //Открываем параметры стиля только для нажатой кнопки
 const styleActivator = (e) => {
-    // Создаем массив из коллекции класса .alpaca__configurator-style
-    array = Array.from(styleElements)
     // Каждому элементу скрываем параметры стилей
-    array.forEach(j => j.classList.add('hidden'))
+    styleElements.forEach(j => j.classList.add('hidden'))
     // Показываем параметры стиля определенного индекса (нажатой кнопки)
-    array[e].classList.remove('hidden')
+    styleElements[e].classList.remove('hidden')
 }
 
-// Делаем "активный" стиль кнопки при нажатии
-const buttonActivator = (e) => {
-    // Наполняем массив кнопками из коллекции дочерних элементов класса .alpaca__configurator-elements
-    let array = []
-    for (let node of configElements.childNodes) {
-        if (node.nodeName != '#text') {
-            array.push(node)
-        }
-    }
-    // Снимаем "активность" со всех кнопок
-    array.forEach(j => j.classList.remove('button--active'))
+// Делаем "активный" стиль кнопки конфигуратора при нажатии
+const configButtonActivator = (e) => {
+    // Снимаем "активность" со всех кнопок    
+    configElements.forEach(j => j.classList.remove('button--active'))
     // Активируем кнопку по индексу массива
-    array[e].classList.add('button--active')
+    configElements[e].classList.add('button--active')
+}
+
+// Делаем "активный" стиль кнопки стилизации при нажатии
+const styleButtonActivator = (e, styleButtonArray) => {
+    // Снимаем "активность" со всех кнопок  
+    styleButtonArray.forEach(j => j.classList.remove('button--active'))
+    // Активируем кнопку по индексу массива
+    styleButtonArray[e].classList.add('button--active')
 }
 
 // Listeners для кнопок конфигуратора элементов
-hair.addEventListener('click', () => {
-    // Открываем и скрываем стили
-    styleActivator(0)
-    // Стилизуем
-    buttonActivator(0)
-})
-ears.addEventListener('click', () => {
-    // Открываем и скрываем стили
-    styleActivator(1)
-    // Стилизуем
-    buttonActivator(1)
-})
-eyes.addEventListener('click', () => {
-    // Открываем и скрываем стили
-    styleActivator(2)
-    // Стилизуем
-    buttonActivator(2)
-})
-mouth.addEventListener('click', () => {
-    // Открываем и скрываем стили
-    styleActivator(3)
-    // Стилизуем
-    buttonActivator(3)
-})
-neck.addEventListener('click', () => {
-    // Открываем и скрываем стили
-    styleActivator(4)
-    // Стилизуем
-    buttonActivator(4)
-})
-leg.addEventListener('click', () => {
-    // Открываем и скрываем стили
-    styleActivator(5)
-    // Стилизуем
-    buttonActivator(5)
-})
-accessories.addEventListener('click', () => {
-    // Открываем и скрываем стили
-    styleActivator(6)
-    // Стилизуем
-    buttonActivator(6)
-})
-background.addEventListener('click', () => {
-    // Открываем и скрываем стили
-    styleActivator(7)
-    // Стилизуем
-    buttonActivator(7)
-})
 
+for (let i = 0; i < configElements.length; i++) {
+    configElements[i].addEventListener('click', () => {
+        // Открываем и скрываем стили
+        styleActivator(i)
+        // Стилизуем
+        styleButtonActivator(i, configElements)
+    })
+}
 
+// Стилизуем кнопки стилей и активируем соответствующий элемент на картинке
+for (let i = 0; i < styleElementsHairArray.length; i++) {
+    styleElementsHairArray[i].addEventListener('click', () => {
+        // Стилизуем
+        styleButtonActivator(i, styleElementsHairArray)
+    })
+}
 
+for (let i = 0; i < styleElementsEarsArray.length; i++) {
+    styleElementsEarsArray[i].addEventListener('click', () => {
+        // Стилизуем
+        styleButtonActivator(i, styleElementsEarsArray)
+    })
+}
+for (let i = 0; i < styleElementsEyesArray.length; i++) {
+    styleElementsEyesArray[i].addEventListener('click', () => {
+        // Стилизуем
+        styleButtonActivator(i, styleElementsEyesArray)
+    })
+}
+for (let i = 0; i < styleElementsMouthArray.length; i++) {
+    styleElementsMouthArray[i].addEventListener('click', () => {
+        // Стилизуем
+        styleButtonActivator(i, styleElementsMouthArray)
+    })
+}
+for (let i = 0; i < styleElementsNeckArray.length; i++) {
+    styleElementsNeckArray[i].addEventListener('click', () => {
+        // Стилизуем
+        styleButtonActivator(i, styleElementsNeckArray)
+    })
+}
+for (let i = 0; i < styleElementsLegArray.length; i++) {
+    styleElementsLegArray[i].addEventListener('click', () => {
+        // Стилизуем
+        styleButtonActivator(i, styleElementsLegArray)
+    })
+}
+for (let i = 0; i < styleElementsAccessoriesArray.length; i++) {
+    styleElementsAccessoriesArray[i].addEventListener('click', () => {
+        // Стилизуем
+        styleButtonActivator(i, styleElementsAccessoriesArray)
+    })
+}
+for (let i = 0; i < styleElementsBackgroundArray.length; i++) {
+    styleElementsBackgroundArray[i].addEventListener('click', () => {
+        // Стилизуем
+        styleButtonActivator(i, styleElementsBackgroundArray)
+    })
+}
